f77204afd867052fa423653bd33d19f05704e492
Strikeskids <github@strikeskids.com>
2016-03-01 21:31:50 -0500

    Scaffolding for ArrayBuffer module

---
+++ src/ArrayBuffer.elm
@@ -0,0 +1,61 @@
+module ArrayBuffer
+    ( ArrayBuffer
+    , Encoding(..), encode, decode
+    , length
+    ) where
+{-| This library intends to give access to the binary ArrayBuffer JS
+interface.
+
+@docs ArrayBuffer, Encoding
+
+@docs encode, decode, length
+-}
+
+import Native.ArrayBuffer
+
+{-| Represent raw buffer values. Equivalent to `ArrayBuffer` in JS
+-}
+type ArrayBuffer = ArrayBuffer
+
+{-| The different encodings for converting between `String` and `ArrayBuffer`
+-}
+type Encoding
+    = Utf8
+    | Hex
+    | Utf16Be
+    | Utf16Le
+
+{-| Encode a `String` into an `ArrayBuffer`. If the encoding fails, then 
+`Nothing` is returned. Otherwise, `Just ArrayBuffer` corresponding to the
+encoded string.
+
+    encode Utf8 "Hello World" -- ArrayBuffer representing "Hello World"
+
+    encode Hex "Hello" -- Nothing
+
+-}
+encode : Encoding -> String -> Maybe ArrayBuffer
+encode =
+    Native.ArrayBuffer.encode
+
+{-| Decode an `ArrayBuffer` into a `String`. If the decoding fails, then
+`Nothing` is returned. Otherwise, `Just String` corresponding to the decoded
+`ArrayBuffer`.
+
+    decode Hex (encode Utf8 "Hello") -- Just "48656b6b6f"
+
+    decode Utf8 (encode Hex "48656b6b6f") -- Just "Hello"
+
+-}
+decode : Encoding -> ArrayBuffer -> Maybe String
+decode =
+    Native.ArrayBuffer.decode
+
+{-| Find the length of an `ArrayBuffer`
+    
+    length (decode Utf8 "Hello") -- 6
+
+-}
+length : ArrayBuffer -> Int
+length = 
+    Native.ArrayBuffer.length
