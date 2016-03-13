7227516457daf9c104db92a088798ccfb83dee18
Strikeskids <github@strikeskids.com>
2016-03-13 10:44:08 -0700

    Add generic encryption methods to Native.Crypto

--- src/Native/Crypto.js
+++ src/Native/Crypto.js
@@ -9,6 +9,7 @@ Elm.Native.Crypto.make = function(localRuntime) {
 
     var Task = Elm.Native.Task.make(localRuntime)
     var Maybe = Elm.Maybe.make(localRuntime)
+    var NativeList = Elm.Native.List.make(localRuntime)
 
     var digestAlgorithmCtor = {
         'Sha1': 'SHA-1',
@@ -20,14 +21,19 @@ Elm.Native.Crypto.make = function(localRuntime) {
     return localRuntime.Native.Crypto.values = {
         digest: F2(digest),
         randomData: randomData,
+        encrypt: F3(encrypt),
+        decrypt: F3(decrypt),
+        generateKey: F3(generateKey),
+        importKey: F5(generateKey),
+        exportKey: F2(generateKey),
     }
 
     function digest(algoType, buf) {
         return Task.asyncFunction(function (callback) {
             crypto.subtle.digest(digestAlgorithmCtor[algoType.ctor], buf)
-                .then(function(hash) {
+                .then(function (hash) {
                     callback(Task.succeed(hash))
-                }, function() {
+                }, function () {
                     callback(Task.fail({ ctor: 'UnknownCryptoError' }))
                 })
         })
@@ -41,5 +47,48 @@ Elm.Native.Crypto.make = function(localRuntime) {
         } catch (e) {
             return Maybe.Nothing
         }
+    }
+
+    function encrypt(options, key, data) {
+        return wrapPromise(function () {
+            return crypto.subtle.encrypt(options, key, data)
+        })
+    }
+
+    function decrypt(options, key, data) {
+        return wrapPromise(function () {
+            return crypto.subtle.decrypt(options, key, data)
+        })
+    }
+
+    function generateKey(options, extractable, actions) {
+        return wrapPromise(function () {
+            return crypto.subtle.generateKey(options, extractable,
+                NativeList.toArray(actions))
+        })
+    }
+
+    function importKey(format, options, extractable, actions, data) {
+        return wrapPromise(function () {
+            return crypto.subtle.importKey(format, data, options, extractable,
+                NativeList.toArray(actions))
+        })
+    }
+
+    function exportKey(format, key) {
+        return wrapPromise(function () {
+            return crypto.subtle.exportKey(format, key)
+        })
+    }
+
+    function wrapPromise(prepare) {
+        return Task.asyncFunction(function (callback) {
+            prepare()
+                .then(function (result) {
+                    callback(Task.succeed(result))
+                }, function () {
+                    callback(Task.fail({ ctor: 'UnknownCryptoError'}))
+                })
+        })
     }
 }
